97a82419200baa8b9353d9a5ab2a2238bc088307
Daria Kozhevina <d.kozhevina0616@yandex.ru>
2026-08-31 17:04:20 +0400

    Выполнено 3 домашнее задание (ORM1) - БД телефоны

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=50, unique=True)
+
